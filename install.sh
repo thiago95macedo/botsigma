2bd8933a93196310cbff2d0bed858e9f79f18927
Thiago Macêdo <thiago95macedo@gmail.com>
2023-01-02 14:44:01 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt install python3-venv
+
+sudo apt install postgresql postgresql-contrib
+sudo -u postgres psql -e --command "CREATE USER sigma WITH SUPERUSER PASSWORD 'sigma1q2w3e4r'"
+sudo -u postgres createdb botsigma
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requeriments.txt
+
+django-admin startproject botsigma .
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver 127.0.0.1:1995
+
