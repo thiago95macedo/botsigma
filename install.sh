54951472a9e39571be9ac3f645c9300c5dda8c29
Thiago Macêdo <thiago95macedo@gmail.com>
2023-01-03 14:32:14 +0000

    Dependencias iqoptionapi

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 pip install --upgrade pip
+pip install --upgrade setuptools
 pip install -r requeriments.txt
 
 django-admin startproject botsigma .
